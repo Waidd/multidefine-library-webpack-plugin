e9dad9f4785f5ec5c14ff5041c3b89a76269c5b3
Nicolas Deveaud <nicolas@deveaud.fr>
2017-03-21 15:09:05 +0100

    Post review

--- MultidefineLibraryWebpackPlugin.js
+++ MultidefineLibraryWebpackPlugin.js
@@ -6,44 +6,20 @@ const DEFINE_EXPORTS_VALUES = {
 };
 
 class MultidefineLibraryWebpacklugin {
-  constructor (exposeList, options) {
-    this.options = options || {};
+  constructor (modulesToExpose, options) {
+    this.options = Object.assign({
+      deprecationMethodName: 'deprecated'
+    }, options);
 
-    if (!exposeList) {
+    if (!modulesToExpose) {
       throw new Error('List of modules to expose should be provided');
     }
 
-    if (Array.isArray(exposeList)) {
-      let namesIndex = {};
-      this.exposeList = exposeList.reduce((computedList, module) => {
-        if (computedList[module.path]) {
-          throw new Error('Duplicated exposed module path "' + module.path + '"');
-        }
-
-        if (namesIndex[module.name]) {
-          throw new Error('Conflict on exposed module name "' + module.name + '"');
-        }
-
-        if (module.aliases) {
-          module.aliases.forEach((alias) => {
-            if (namesIndex[alias]) {
-              throw new Error('Conflict on exposed module alias "' + alias + '"');
-            }
-
-            namesIndex[alias] = module.name;
-          });
-        }
-
-        namesIndex[module.name] = module.name;
-        computedList[module.path] = {
-          name: module.name,
-          type: module.type,
-          aliases: module.aliases,
-          deprecated: module.deprecated
-        };
-        return computedList;
-      }, {});
+    if (Array.isArray(modulesToExpose)) {
+      modulesToExpose = this._convertArrayToObject(modulesToExpose);
     }
+
+    this.modulesToExpose = modulesToExpose;
   }
 
   apply (compiler) {
@@ -55,6 +31,43 @@ class MultidefineLibraryWebpacklugin {
     });
   }
 
+  _addModuleToList (module, list, namesIndex) {
+    if (list[module.path]) {
+      throw new Error('Duplicated exposed module path "' + module.path + '"');
+    }
+
+    if (namesIndex[module.name]) {
+      throw new Error('Conflict on exposed module name "' + module.name + '"');
+    }
+
+    if (module.aliases) {
+      // We use a temporary index here to avoid touching global names index
+      // before we have checked all aliases
+      let aliasesIndex = {};
+
+      module.aliases.forEach((alias) => {
+        if (namesIndex[alias]) {
+          throw new Error('Conflict on exposed module alias "' + alias + '"');
+        }
+
+        aliasesIndex[alias] = module.name;
+      });
+
+      Object.assign(namesIndex, aliasesIndex);
+    }
+
+    namesIndex[module.name] = module.name;
+
+    return Object.assign({}, list, { [module.path]: module });
+  }
+
+  _convertArrayToObject (modulesToExpose) {
+    const namesIndex = {};
+    return modulesToExpose.reduce((modulesList, module) =>
+      this._addModuleToList(module, modulesList, namesIndex),
+      {});
+  }
+
   _getRelativePath (resource) {
     resource = resource.slice(this.context.length);
     if (resource.startsWith('/')) { resource = resource.slice(1); }
@@ -63,53 +76,53 @@ class MultidefineLibraryWebpacklugin {
   }
 
   _appendDefine (module) {
-    let modulePath = module.resource && this._getRelativePath(module.resource);
+    const modulePath = module.resource && this._getRelativePath(module.resource);
     if (!modulePath) { return; }
 
-    let expose = this.exposeList[modulePath];
-    if (!expose) { return; }
+    const moduleToExpose = this.modulesToExpose[modulePath];
+    if (!moduleToExpose) { return; }
 
-    let isAMD = module._source._value.indexOf('define(') !== -1;
-    let isCommonJS = module._source._value.indexOf('module.exports') !== -1 && !isAMD;
+    const isAMD = module._source._value.indexOf('define(') !== -1;
+    const isCommonJS = module._source._value.indexOf('module.exports') !== -1 && !isAMD;
 
-    if (!expose.type) {
-      expose.type = isCommonJS ? 'commonjs' : 'amd';
-    } else if ((expose.type === 'commonjs') !== isCommonJS) {
-      console.warn(`Provided module type does not match auto-detection result for module ${expose.name}`);
-      console.warn('provided', expose.type);
+    if (!moduleToExpose.type) {
+      moduleToExpose.type = isCommonJS ? 'commonjs' : 'amd';
+    } else if ((moduleToExpose.type === 'commonjs') !== isCommonJS) {
+      console.warn(`Provided module type does not match auto-detection result for module ${moduleToExpose.name}`);
+      console.warn('provided', moduleToExpose.type);
       console.warn('detected', isCommonJS ? 'commonjs' : 'amd');
     }
 
-    if (!DEFINE_EXPORTS_VALUES[expose.type]) {
-      throw new Error(`Unsupported module type "${expose.type}"`);
+    if (!DEFINE_EXPORTS_VALUES[moduleToExpose.type]) {
+      throw new Error(`Unsupported module type "${moduleToExpose.type}"`);
     }
 
     const deprecationMethodName = this.options.deprecationMethodName || 'deprecated';
     let deprecationCall = '';
 
     if (exposed.deprecated) {
-      let deprecationMsg = `Deprecated module usage: "${expose.name}"`;
+      let deprecationMsg = `Deprecated module usage: "${moduleToExpose.name}"`;
 
       deprecationCall+= `window.${deprecationMethodName} && ` +
         `window.${deprecationMethodName}('${deprecationMsg}');`;
     }
 
-    module._source._value += `\ndefine('${expose.name}', function () {` +
+    module._source._value += `\ndefine('${moduleToExpose.name}', function () {` +
       ` ${deprecationCall}` +
-      ` return ${DEFINE_EXPORTS_VALUES[expose.type]};` +
+      ` return ${DEFINE_EXPORTS_VALUES[moduleToExpose.type]};` +
       '});\n';
 
-    if (expose.aliases) {
-      expose.aliases.forEach((alias) => {
+    if (moduleToExpose.aliases) {
+      moduleToExpose.aliases.forEach((alias) => {
         const deprecationMsg = `Deprecated module alias "${alias}" used. ` +
-          `Please use real module name "${expose.name}"`;
+          `Please use real module name "${moduleToExpose.name}"`;
 
         deprecationCall+= `window.${deprecationMethodName} && ` +
           `window.${deprecationMethodName}('${deprecationMsg}');`;
 
         module._source._value += `\ndefine('${alias}', function () {` +
           ` ${deprecationCall}` +
-          ` return ${DEFINE_EXPORTS_VALUES[expose.type]};` +
+          ` return ${DEFINE_EXPORTS_VALUES[moduleToExpose.type]};` +
           '});\n';
       });
     }
